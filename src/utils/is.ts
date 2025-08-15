a409453844f44e74dc76cd23311833f0728e71e6
Mikita Taukachou <edloidas@gmail.com>
2025-08-15 17:02:37 +0300

    Add customizable left and right parts of ListItem #42

--- src/utils/is.ts
+++ src/utils/is.ts
@@ -1,3 +1,9 @@
+import { type ComponentType, isValidElement, type ReactElement, type ReactNode } from 'react';
+
 export function isFunction(value: unknown): value is (...args: unknown[]) => unknown {
   return typeof value === 'function';
+}
+
+export function isOfType<P>(node: ReactNode, Comp: ComponentType<P>): node is ReactElement<P> {
+  return isValidElement(node) && (node as ReactElement<P>).type === Comp;
 }
