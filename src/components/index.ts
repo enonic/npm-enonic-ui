51562917441d965419d2080e14af1b876c0096c4
Mikita Taukachou <edloidas@gmail.com>
2025-08-21 17:43:57 +0300

    Fix missing components root imports

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,4 +2,6 @@ export * from './button';
 export * from './checkbox';
 export * from './icon-button';
 export * from './input';
+export * from './list-item';
+export * from './selectable-list-item';
 export * from './tooltip';
